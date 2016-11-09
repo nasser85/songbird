075111f0abac1dd3d76755ecb2eb804eec547d76
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2016-11-08 21:23:18 -0500

    cleaned up console.logs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var indexPath = path.join(__dirname, './index.html');
 var publicPath = path.join(__dirname, './public');
 var apiPath = path.join(__dirname, './api.js');
 var bodyParser = require('body-parser');
-var lyricist = require('lyricist')('y6VxCEfzLIGleplTUmkM3_Xgpv5sb6aHk1iH7d7wzEwGuQBfK-9Ai9M6EhaS7EJG');
 
 var app = Express();
 
@@ -20,14 +19,4 @@ app.use('/*', function(req, res) {
 });
 app.listen(3001, function() {
 	console.log("Server is up on port 3001!")
-});
-
-console.log(lyricist);
-lyricist.song(1, function (err, song) {
-	if (err) {
-		console.log(err)
-	} else {
-		  console.log(song.lyrics);
-	}
-
 });
